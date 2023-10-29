7f2d84544fd39f87b79552d3c75cec04cf56f5cf
Abdillahi485 <daakircadde143@gmail.com>
2023-10-29 13:41:04 +0300

    soved football points

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculatePoints( wins,draw,loss){
+return (wins *3 + draw *1 + loss*0)
+}
+
+let total=calculatePoints(5,2,3);
+console.log(total)
